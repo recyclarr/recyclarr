48aeb2d7c504189172c19e00a94ef8df2617c9ea
Robert Dailey <rcdailey@gmail.com>
2021-04-19 17:23:50 -0500

    test: Unit tests for some TestLibrary classes

---
+++ src/TestLibrary.Tests/StreamBuilderTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestLibrary.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class StreamBuilderTest
+    {
+        [Test]
+        public void FromString_UsingString_ShouldOutputSameString()
+        {
+            var stream = StreamBuilder.FromString("test");
+            stream.ReadToEnd().Should().Be("test");
+        }
+    }
+}
---
+++ src/TestLibrary.Tests/StringUtilsTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TestLibrary.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class StringUtilsTest
+    {
+        [Test]
+        public void TrimmedString_Newlines_AreStripped()
+        {
+            var testStr = "\r\ntest\r\n";
+            StringUtils.TrimmedString(testStr).Should().Be("test");
+        }
+    }
+}
