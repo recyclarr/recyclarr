fadd8bd5de9ad8e03699174a9fa375d818607118
Robert Dailey <rcdailey@gmail.com>
2022-01-21 19:48:46 -0600

    test: Clean up composition root tests

--- src/Trash.Tests/CompositionRootTest.cs
+++ src/Trash.Tests/CompositionRootTest.cs
@@ -12,23 +12,35 @@ namespace Trash.Tests;
 
 public record ServiceFactoryWrapper(Type Service, Action<ILifetimeScope> Instantiate);
 
+public static class FactoryForService<TService>
+{
+    public static ServiceFactoryWrapper WithArgs<TP1>(TP1 arg1 = default!)
+    {
+        return new ServiceFactoryWrapper(typeof(TService),
+            c => c.Resolve<Func<TP1, TService>>().Invoke(arg1));
+    }
+}
+
 [TestFixture]
 [Parallelizable(ParallelScope.All)]
 public class CompositionRootTest
 {
-    private static class FactoryForService<TService>
-    {
-        public static ServiceFactoryWrapper WithArgs<TP1>(TP1 arg1 = default!)
-        {
-            return new ServiceFactoryWrapper(typeof(TService),
-                c => c.Resolve<Func<TP1, TService>>().Invoke(arg1));
-        }
-    }
-
     private static readonly List<ServiceFactoryWrapper> FactoryTests = new()
     {
         FactoryForService<IGitRepository>.WithArgs("path")
     };
+
+    [TestCaseSource(typeof(CompositionRootTest), nameof(FactoryTests))]
+    public void Service_requiring_factory_should_be_instantiable(ServiceFactoryWrapper service)
+    {
+        var act = () =>
+        {
+            using var container = CompositionRoot.Setup();
+            service.Instantiate(container);
+        };
+
+        act.Should().NotThrow();
+    }
 
     private sealed class ConcreteTypeEnumerator : IEnumerable
     {
@@ -44,28 +56,19 @@ public class CompositionRootTest
             return _container.ComponentRegistry.Registrations
                 .SelectMany(x => x.Services)
                 .OfType<TypedService>()
-                .Where(x => FactoryTests.All(y => y.Service != x.ServiceType))
+                .Select(x => x.ServiceType)
+                .Distinct()
+                .Except(FactoryTests.Select(x => x.Service))
+                .Where(x => x.FullName == null || !x.FullName.StartsWith("Autofac."))
                 .GetEnumerator();
         }
     }
 
-    [TestCaseSource(typeof(CompositionRootTest), nameof(FactoryTests))]
-    public void Service_requiring_factory_should_be_instantiable(ServiceFactoryWrapper service)
-    {
-        var act = () =>
-        {
-            using var container = CompositionRoot.Setup();
-            service.Instantiate(container);
-        };
-
-        act.Should().NotThrow();
-    }
-
     [TestCaseSource(typeof(ConcreteTypeEnumerator))]
-    public void Service_should_be_instantiable(Service service)
+    public void Service_should_be_instantiable(Type service)
     {
         using var container = CompositionRoot.Setup();
-        container.Invoking(c => c.ResolveService(service))
+        container.Invoking(c => c.Resolve(service))
             .Should().NotThrow()
             .And.NotBeNull();
     }
