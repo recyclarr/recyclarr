418f0f6de9a2f1bbfa140a4a741aec84d1ce66ee
Robert Dailey <rcdailey@gmail.com>
2022-02-12 14:05:17 -0600

    ci: Move smoke test to its own job

--- ci/SmokeTest.ps1
+++ ci/SmokeTest.ps1
@@ -1,18 +1,18 @@
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
-    [string] $runtime
+    [string] $PathToTrashExe
 )
 
 $ErrorActionPreference = "Stop"
 
 if (Get-Command chmod -errorAction SilentlyContinue) {
     "The chmod command was found. Setting read + execute permission."
-    & chmod +rx ./publish/$runtime/trash
+    & chmod +rx $PathToTrashExe
 }
 
 "Execute trash command to ensure basic functionality is working"
-& .\publish\$runtime\trash -h
+& $PathToTrashExe -h
 if ($LASTEXITCODE -ne 0) {
     "Trash executable failed to run with exit code: $LASTEXITCODE"
     exit -1
