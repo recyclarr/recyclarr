d4b0d43ba64368d9de713330b2e7c510f5fb461e
Jonas <jonas@skayo.dev>
2022-04-11 21:08:23 +0200

    Added Dockerfile and updated build workflow

---
+++ docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# If the script has any arguments, invoke the CLI instead.
+# This allows for "docker run rcdailey/trash-updater create-config", "docker run rcdailey/trash-updater --help", etc.
+if [ "$#" -gt 0 ]; then
+	/usr/local/bin/trash "$@"
+else
+	schedule=${CRON_SCHEDULE:-@daily}
+	sonarr_schedule=${CRON_SCHEDULE_SONARR:-$schedule}
+	radarr_schedule=${CRON_SCHEDULE_RADARR:-$schedule}
+
+	config_file=${CONFIG_FILE:-/config/trash.yml}
+
+	echo "Creating crontab file..."
+	echo "$sonarr_schedule /usr/local/bin/trash sonarr --config $config_file" > /var/spool/cron/crontabs/root
+	echo "$radarr_schedule /usr/local/bin/trash radarr --config $config_file" >> /var/spool/cron/crontabs/root
+
+	echo "Starting cron daemon..."
+	exec crond -f
+fi
