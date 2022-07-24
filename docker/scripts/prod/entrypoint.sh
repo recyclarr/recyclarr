7fa36b9c8c3fbde414a6ca943909c670a79d4415
Robert Dailey <rcdailey@gmail.com>
2022-07-24 11:34:14 -0500

    chore(docker): Failure messages if PUID/PGID are still in use

--- docker/scripts/prod/entrypoint.sh
+++ docker/scripts/prod/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+if [[ ! -z ${PUID+x} ]]; then
+    echo 'PUID is no longer supported. Use `--user` instead.'
+    exit 1
+fi
+
+if [[ ! -z ${PGID+x} ]]; then
+    echo 'PGID is no longer supported. Use `--user` instead.'
+    exit 1
+fi
+
 # If the script has any arguments, invoke the CLI instead. This allows the image to be used as a CLI
 # with something like:
 #
