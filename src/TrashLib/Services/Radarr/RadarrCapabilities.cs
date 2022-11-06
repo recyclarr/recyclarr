3df3bc869e0b8fb641652324ae5c0417b4a0924b
Robert Dailey <github@rdailey.me>
2022-11-06 10:38:03 -0600

    refactor: Silence unused Version param warning

--- src/TrashLib/Services/Radarr/RadarrCapabilities.cs
+++ src/TrashLib/Services/Radarr/RadarrCapabilities.cs
@@ -1,5 +1,9 @@
 namespace TrashLib.Services.Radarr;
 
+// ReSharper disable once NotAccessedPositionalProperty.Global
+//
+// May get used one day; keep the parameter around so that calling
+// code does not need to be changed later.
 public record RadarrCapabilities(Version Version)
 {
     public RadarrCapabilities() : this(new Version())
