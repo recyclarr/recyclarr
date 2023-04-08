39f106c72bece446b23300d56f09e96ea33e3b3f
Robert Dailey <git@rdailey.me>
2023-04-07 21:06:26 -0500

    refactor: Fix warning related to FluentValidation & NRT

--- src/Recyclarr.Common/FluentValidation/FluentValidationExtensions.cs
+++ src/Recyclarr.Common/FluentValidation/FluentValidationExtensions.cs
@@ -13,34 +13,12 @@ public static class FluentValidationExtensions
         IValidator<TProperty> validator,
         params string[] ruleSets)
     {
-        var adapter = new NullableChildValidatorAdaptor<T, TProperty>(validator, validator.GetType())
+        var adapter = new ChildValidatorAdaptor<T, TProperty?>(validator!, validator.GetType())
         {
             RuleSets = ruleSets
         };
 
         return ruleBuilder.SetAsyncValidator(adapter);
-    }
-
-    private sealed class NullableChildValidatorAdaptor<T, TProperty> : ChildValidatorAdaptor<T, TProperty>,
-        IPropertyValidator<T, TProperty?>, IAsyncPropertyValidator<T, TProperty?>
-    {
-        public NullableChildValidatorAdaptor(IValidator<TProperty> validator, Type validatorType)
-            : base(validator, validatorType)
-        {
-        }
-
-        public override bool IsValid(ValidationContext<T> context, TProperty? value)
-        {
-            return base.IsValid(context, value!);
-        }
-
-        public override Task<bool> IsValidAsync(
-            ValidationContext<T> context,
-            TProperty? value,
-            CancellationToken cancellation)
-        {
-            return base.IsValidAsync(context, value!, cancellation);
-        }
     }
 
     public static IEnumerable<TSource> IsValid<TSource, TValidator>(
