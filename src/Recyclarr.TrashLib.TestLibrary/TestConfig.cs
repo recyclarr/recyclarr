2652a5c6630b1f00843b0f9d4312277a94c28ed7
Robert Dailey <git@rdailey.me>
2023-04-21 07:31:21 -0500

    build: Move Jetbrains.Annotations to projects

--- src/Recyclarr.TrashLib.TestLibrary/TestConfig.cs
+++ src/Recyclarr.TrashLib.TestLibrary/TestConfig.cs
@@ -1,10 +1,8 @@
-using JetBrains.Annotations;
 using Recyclarr.TrashLib.Config;
 using Recyclarr.TrashLib.Config.Services;
 
 namespace Recyclarr.TrashLib.TestLibrary;
 
-[UsedImplicitly]
 public class TestConfig : ServiceConfiguration
 {
     public override SupportedServices ServiceType => SupportedServices.Sonarr;
