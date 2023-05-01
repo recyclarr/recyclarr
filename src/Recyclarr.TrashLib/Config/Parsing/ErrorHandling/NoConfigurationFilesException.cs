fe9f98e435f6596d0e2c861a96ddf97eebbb3d02
Robert Dailey <git@rdailey.me>
2023-05-01 07:00:30 -0500

    refactor: Make exception class serializable

--- src/Recyclarr.TrashLib/Config/Parsing/ErrorHandling/NoConfigurationFilesException.cs
+++ src/Recyclarr.TrashLib/Config/Parsing/ErrorHandling/NoConfigurationFilesException.cs
@@ -1,9 +1,17 @@
+using System.Runtime.Serialization;
+
 namespace Recyclarr.TrashLib.Config.Parsing.ErrorHandling;
 
+[Serializable]
 public class NoConfigurationFilesException : Exception
 {
     public NoConfigurationFilesException()
         : base("No configuration YAML files found")
+    {
+    }
+
+    protected NoConfigurationFilesException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
     {
     }
 }
