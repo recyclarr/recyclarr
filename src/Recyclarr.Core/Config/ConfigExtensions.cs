7a94e50cef0a16ccca243af21716ceadcbfcaba8
Robert Dailey <git@rdailey.me>
2025-03-02 14:19:24 -0600

    refactor: Remove commented out code

--- src/Recyclarr.Core/Config/ConfigExtensions.cs
+++ src/Recyclarr.Core/Config/ConfigExtensions.cs
@@ -10,42 +10,4 @@ public static class ConfigExtensions
         var radarr = config?.Radarr?.Count ?? 0;
         return sonarr + radarr == 0;
     }
-
-    // public static ICollection<string> GetSplitInstances(this IEnumerable<LoadedConfigYaml> configs)
-    // {
-    //     return configs
-    //         .GroupBy(x => x.Yaml.BaseUrl)
-    //         .Where(x => x.Count() > 1)
-    //         .SelectMany(x => x.Select(y => y.InstanceName))
-    //         .ToList();
-    // }
-
-    // public static IEnumerable<string> GetNonExistentInstanceNames(
-    //     this IEnumerable<LoadedConfigYaml> configs,
-    //     ConfigFilterCriteria criteria
-    // )
-    // {
-    //     if (criteria.Instances is not { Count: > 0 })
-    //     {
-    //         return [];
-    //     }
-    //
-    //     var names = configs.Select(x => x.InstanceName).ToList();
-    //
-    //     return criteria.Instances.Where(x =>
-    //         !names.Contains(x, StringComparer.InvariantCultureIgnoreCase)
-    //     );
-    // }
-
-    // public static IEnumerable<string> GetDuplicateInstanceNames(
-    //     this IReadOnlyCollection<LoadedConfigYaml> configs
-    // )
-    // {
-    //     return configs
-    //         .Select(x => x.InstanceName)
-    //         .GroupBy(x => x, StringComparer.InvariantCultureIgnoreCase)
-    //         .Where(x => x.Count() > 1)
-    //         .Select(x => x.First())
-    //         .ToList();
-    // }
 }
