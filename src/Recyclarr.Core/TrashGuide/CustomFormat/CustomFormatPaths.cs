557e371136518fc7c91c4065cc44a55b438db71f
Robert Dailey <git@rdailey.me>
2025-10-25 20:57:21 -0500

    refactor: remove unused path record types

--- src/Recyclarr.Core/TrashGuide/CustomFormat/CustomFormatPaths.cs
+++
@@ -1,8 +0,0 @@
-using System.IO.Abstractions;
-
-namespace Recyclarr.TrashGuide.CustomFormat;
-
-internal record CustomFormatPaths(
-    IReadOnlyList<IDirectoryInfo> CustomFormatDirectories,
-    IFileInfo CollectionOfCustomFormatsMarkdown
-);
--- src/Recyclarr.Core/TrashGuide/QualitySize/QualitySizePaths.cs
+++
@@ -1,5 +0,0 @@
-using System.IO.Abstractions;
-
-namespace Recyclarr.TrashGuide.QualitySize;
-
-internal record QualitySizePaths(IReadOnlyCollection<IDirectoryInfo> QualitySizeDirectories);
