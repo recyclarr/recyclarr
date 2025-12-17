4807831be7ec4e6c5b72f6146b8575e03c4ec2ef
Robert Dailey <git@rdailey.me>
2025-12-17 07:37:07 -0600

    fix(cli): Remove duplicate command interceptor invocation
    Spectre.Console automatically resolves ICommandInterceptor from the
    TypeRegistrar. Manually calling SetInterceptor caused the interceptor
    to be invoked twice - once from the DI resolution and once from the
    configuration.

--- src/Recyclarr.Cli/Console/CliSetup.cs
+++ src/Recyclarr.Cli/Console/CliSetup.cs
@@ -81,7 +81,6 @@ internal static class CliSetup
 #endif
 
             config.ConfigureConsole(scope.Resolve<IAnsiConsole>());
-            config.SetInterceptor(scope.Resolve<ICommandInterceptor>());
             config.PropagateExceptions();
             config.UseStrictParsing();
 
