e86a263a665b10928dbed70e0283661844228161
Robert Dailey <git@rdailey.me>
2026-02-17 08:19:16 -0600

    refactor: remove unused Noop class

--- src/Recyclarr.Core/Sync/IInstancePublisher.cs
+++ src/Recyclarr.Core/Sync/IInstancePublisher.cs
@@ -11,27 +11,7 @@ public interface IDiagnosticPublisher
 
 public interface IInstancePublisher : IDiagnosticPublisher
 {
-    static IInstancePublisher Noop { get; } = new NoopInstancePublisher();
-
     string Name { get; }
     void SetStatus(InstanceProgressStatus status);
     IPipelinePublisher ForPipeline(PipelineType type);
-}
-
-internal sealed class NoopInstancePublisher : IInstancePublisher
-{
-    public string Name => "";
-
-    public void SetStatus(InstanceProgressStatus status) { }
-
-    public void AddError(string message) { }
-
-    public void AddWarning(string message) { }
-
-    public void AddDeprecation(string message) { }
-
-    public IPipelinePublisher ForPipeline(PipelineType type)
-    {
-        return NoopPipelinePublisher.Instance;
-    }
 }
